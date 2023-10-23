aee60de1b57198b863332236ed8b40095db1d6b0
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-23 07:14:45 +0200

    Added express middleware handler and json parsing

--- backend/app.js
+++ backend/app.js
@@ -6,34 +6,20 @@ const usersRoutes = require('./routes/users-routes');
 
 const app = express();
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
-        address: '20 W 34th St., New York, NY 10001',
-        location: {
-            lat: 40.7484405,
-            long: -73.9878584
-        },
-        creator: 'u1'
-    },
-    {
-        id: 'p2',
-        title: 'Emp. State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        imageUrl: 'https://media.timeout.com/images/101705309/750/422/image.jpg',
-        address: '20 W 34th St., New York, NY 10001',
-        location: {
-            lat: 40.7484405,
-            long: -73.9882393
-        },
-        creator: 'u2'
-    }
-];
+app.use(bodyParser.json());
 
 app.use('/api/places', placesRoutes) // => /api/places/...
 app.use('/api/users', usersRoutes)
+
+// Express error middleware handler (4-args)
+app.use((error, req,res,next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    // If a header has not been sent && a error code is not set
+    // Use 500 which indicates something went wrong
+    res.status(error.code || 500);
+    res.json({message: error.message || 'An unkown error occurred!'});
+})
 
 app.listen(5000);
