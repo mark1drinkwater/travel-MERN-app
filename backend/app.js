55bf0dbc69ee9fe88b04f1f7981bab655f0da699
mark1drinkwater <124071085+mark1drinkwater@users.noreply.github.com>
2023-10-24 07:19:39 +0200

    Added 404 error route

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
@@ -10,6 +11,12 @@ app.use(bodyParser.json());
 
 app.use('/api/places', placesRoutes) // => /api/places/...
 app.use('/api/users', usersRoutes)
+
+// If there's no matching route then this one gets called.
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this route.', 404);
+    throw error;
+})
 
 // Express error middleware handler (4-args)
 app.use((error, req,res,next) => {
